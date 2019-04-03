3da180882e3c066ee8ac8151d28dc2eedcac2a75
Drew <drew.r.wilson@gmail.com>
2019-04-03 17:05:29 -0400

    make it so users can fake session tokens using this script

--- greasemonkey-show-sessionToken.js
+++ greasemonkey-show-sessionToken.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Show cookie as a popup on any StartSOLE page
-// @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  Show sessionToken for a startSole session
+// @name         StartSOLE Debugger
+// @namespace    http://StartSOLE.org
+// @version      1.0
+// @description  Show/edit session token in cookie
 // @author       Drew
 // @match        *://localhost:8080/*
 // @match        *://app.startsole.org/*
@@ -10,25 +10,35 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function($) {
     'use strict';
-
-    var toastHTML2 = "<span>";
-
-    if (!document.cookie) {
-        //no token defined, so output this
-        toastHTML2 += 'You are currently logged out. No sessionToken currently defined.';
-    } else {
-        var sessionToken = document.cookie.split(';').filter(function(item) {
+    var sessionToken = '';
+    if (document.cookie) {
+      try {
+        sessionToken = document.cookie.split(';').filter(function(item) {
             return item.trim().indexOf('sessionToken=') == 0
-        })[0];
-        //token is defined, so output it
-            //show cookie:
-
-        toastHTML2 += 'Your current sessionToken from the cookie is ' + sessionToken;
+        })[0].slice(14);
+      } catch {
+        sessionToken = ''
+      }
     }
 
-    toastHTML2 += '</span>';
-    toastHTML2 += '<button class="btn-flat toast-action" onClick="M.Toast.dismissAll();">Dismiss all</button>';
-    M.toast({html: toastHTML2, displayLength: 10000});
-})();
+    var adminPanelHtml = '<div id="admin-panel" style="background: #ddd; z-index: 9999; position: fixed; top: 150px;right: 0;width: 350px; min-height: 100px; padding: 15px; border-radius: 5px;" class="center">ADMIN PANEL<hr><br><div class="row"><form class="col s12"><input id="sessionToken" type="text" placeholder="no session token" value="' + sessionToken + '"><label for="sessionToken">session token</label><br><br><button type="submit" class="btn" onClick="setSessionTokenInCookie(event);">Update Session Token</button></form></div><a id="admin-panel-hide-button" href="#" style="font-size: .75em">hide</a></div>';
+    var adminElement = document.createElement('div');
+    adminElement.innerHTML = adminPanelHtml;
+    document.body.append(adminElement);
+
+$("#admin-panel").submit(function (event) {
+    event.preventDefault();
+    var sessionTokenNew = $('#sessionToken').val();
+    document.cookie = 'sessionToken=; Max-Age=0';//remove the sessionToken from the cookie
+    document.cookie = 'sessionToken='+sessionTokenNew; //save the sessionToken in the cookie
+    location.reload();
+  });
+  $("#admin-panel-hide-button").click(function (event) {
+    event.preventDefault();
+    $("#admin-panel").hide();
+  })
+
+
+})(jQuery)
