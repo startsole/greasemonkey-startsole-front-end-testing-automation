6e139d2ea6c41a29cb6b4cb660315e0800b7b713
Drew Wilson <drewrwilson@users.noreply.github.com>
2019-04-24 11:21:24 -0400

    Add ability to change language in cookie

--- greasemonkey-show-sessionToken.js
+++ greasemonkey-show-sessionToken.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         StartSOLE Debugger
 // @namespace    http://StartSOLE.org
-// @version      1.0
+// @version      1.1
 // @description  Show/edit session token in cookie
 // @author       Drew
 // @match        *://localhost:8080/*
@@ -12,27 +12,48 @@
 
 (function($) {
     'use strict';
+    //get the sessionToken & language from the cookie
     var sessionToken = '';
+    var language = 'en'; //default
     if (document.cookie) {
       try {
         sessionToken = document.cookie.split(';').filter(function(item) {
             return item.trim().indexOf('sessionToken=') == 0
         })[0].slice(14);
       } catch {
-        sessionToken = ''
+        sessionToken = '';
       }
+
+      try {
+        language = document.cookie.split(';').filter(function(item) {
+            return item.trim().indexOf('language=') == 0
+        })[0].slice(10);
+      } catch {
+        language = 'en';
+      }
+
     }
 
-    var adminPanelHtml = '<div id="admin-panel" style="background: #ddd; z-index: 9999; position: fixed; top: 150px;right: 0;width: 350px; min-height: 100px; padding: 15px; border-radius: 5px;" class="center">ADMIN PANEL<hr><br><div class="row"><form class="col s12"><input id="sessionToken" type="text" placeholder="no session token" value="' + sessionToken + '"><label for="sessionToken">session token</label><br><br><button type="submit" class="btn" onClick="setSessionTokenInCookie(event);">Update Session Token</button></form></div><a id="admin-panel-hide-button" href="#" style="font-size: .75em">hide</a></div>';
+    var adminPanelHtml = '<div id="admin-panel" style="background: #ddd; z-index: 9999; position: fixed; top: 150px;right: 0;width: 350px; min-height: 100px; padding: 15px; border-radius: 5px;" class="center">ADMIN PANEL <hr> <form class="row"> <div class="input-field col s12"> <input id="sessionToken" type="text" placeholder="no session token" value="no sessionToken"> <label for="sessionToken" class="active">session token</label> </div> <div class="input-field col s12"> <select id="language"> <option value="" disabled selected>Choose your language</option> <option value="en">English</option> <option value="es">Spanish (Colombia)</option> </select> <label for="language">Language</label> </div> <button type="submit" class="btn">Update Cookie</button> </form> <a id="admin-panel-hide-button" href="#" style="font-size: .75em">hide</a> </div>';
     var adminElement = document.createElement('div');
     adminElement.innerHTML = adminPanelHtml;
     document.body.append(adminElement);
-
+    $('#sessionToken').val(sessionToken); //add sessionToken to admin panel from cookie
+    $('#language').val(language); //add language to admin panel from cookie
+    //initiliaze dropdown
+    $(document).ready(function(){
+        $('select').formSelect();
+  });
 $("#admin-panel").submit(function (event) {
     event.preventDefault();
+    //update cookie with data from form
     var sessionTokenNew = $('#sessionToken').val();
+    var language = $('#language').val();
+
     document.cookie = 'sessionToken=; Max-Age=0';//remove the sessionToken from the cookie
     document.cookie = 'sessionToken='+sessionTokenNew; //save the sessionToken in the cookie
+    document.cookie = 'language=; Max-Age=0';//remove the language from the cookie
+    document.cookie = 'language='+language; //save the language in the cookie
     location.reload();
   });
   $("#admin-panel-hide-button").click(function (event) {
